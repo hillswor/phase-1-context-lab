1472321297d1f4c277c17ec67688185b18385edc
Bruce <bruce.hillsworth@gmail.com>
2023-04-02 13:05:59 -0600

    Added createEmployeeRecord and createEmployeeRecords functions

--- index.js
+++ index.js
@@ -1,23 +1,40 @@
 /* Your Code Here */
 
 /*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+ We're giving you this function. Take a look at it, you might see some usage that's new and different. That's because we're avoiding a well-known, but sneaky bug that we'll cover in the next few lessons!
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
+ As a result, the lessons for this function will pass *and* it will be available for you to use if you need it!
  */
+function createEmployeeRecord(employeeData) {
+    const employee = {
+        firstName: employeeData[0],
+        familyName: employeeData[1],
+        title: employeeData[2],
+        payPerHour: employeeData[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    };
+    return employee;
+}
+
+function createEmployeeRecords(employeeData) {
+    const employees = employeeData.map((employee) => {
+        return createEmployeeRecord(employee);
+    });
+    return employees;
+}
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
-        return e.date
-    })
+        return e.date;
+    });
 
-    const payable = eligibleDates.reduce(function (memo, d) {
-        return memo + wagesEarnedOnDate.call(this, d)
-    }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
+    const payable = eligibleDates.reduce(
+        function (memo, d) {
+            return memo + wagesEarnedOnDate.call(this, d);
+        }.bind(this),
+        0
+    ); // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
-    return payable
-}
-
+    return payable;
+};
